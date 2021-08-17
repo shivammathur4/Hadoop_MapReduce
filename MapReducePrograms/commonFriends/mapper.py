8cbea3ca76f961b9c3a55dea8ba2549304c8b09f
shivammmathur4 <shivamm.4aug@gmail.com>
2021-08-17 20:15:22 +0530

    [Shivam] Add : common friends map reduce code

--- MapReducePrograms/commonFriends/mapper.py
+++ MapReducePrograms/commonFriends/mapper.py
@@ -8,17 +8,38 @@
 
   
 import sys
-import re
- 
-#--- get all lines from stdin ---
+  
+# reading entire line from STDIN (standard input)
+friend_key = []
 for line in sys.stdin:
+    # to remove leading and trailing whitespace
     line = line.strip()
+    # split the line into words
+    words = line.split(":")
+
+    # a = b c d f
+    # a map with every friend  ab ac ad af = b c d f
+    # [a , [b c d f]]
+    final_friend_list = []
     
-    word, sign, values = re.split(r'(=)', line)
-    values = (values)[2:-1]
-    values = values.split(',')
-    for letter in values:
-        print(word, letter.strip())
+    for i in words:
+        friend_list = i.split()
+        final_friend_list.append(friend_list) # [[a], [b,c,d,f]]
+        
+    for i in final_friend_list[0]:
+        for j in final_friend_list[1]:
+            if [j,i] in friend_key:
+                final_text = j+i
+            elif [i,j] in friend_key:
+                final_text = i+j
+            else:
+                friend_key.append([j,i])  #ab
+                final_text = j+i
+            
+            #print(final_text," ", final_friend_list[1])
+            print('%s\t%s' % (final_text, final_friend_list[1]))
+            
+            
         
             
             
--- MapReducePrograms/commonFriends/reducer.py
+++ MapReducePrograms/commonFriends/reducer.py
@@ -7,27 +7,25 @@
 
 #! /home/shivammathur/Downloads/hadoop/Hadoop_MapReduce/MapReducePrograms/commonFriends
   
-import sys
- 
-# maps words to their counts
-word_dict = {}
-word_list = []
-count = 0
-i=0
-j=1
-# input comes from STDIN
+from operator import itemgetter
+import sys  
+import ast
+
+friend_data = {}
 for line in sys.stdin:
     # remove leading and trailing whitespace
     line = line.strip()
-    
-    # parse the input we got from mapper.py
-    word, friend = line.split()
-    word_dict.setdefault(word, []).append(friend)
-    
-count = len(word_dict.keys())
-keys = (list(word_dict))
-while(j< count):
-    print(keys[i], keys[j], list(set(word_dict[keys[i]]) & set(word_dict[keys[j]])))
-    i = i + 1
-    j = j + 1
-    
+    # slpiting the data on the basis of tab we have provided in mapper.py
+    word, count = line.split('\t')
+    if word not in friend_data:
+        friend_data[word] = []
+    count = ast.literal_eval(count)
+
+    friend_data[word].append(count)
+
+for key in friend_data:
+    if len(friend_data[key]) > 1:
+        inter = list(set(friend_data[key][0]) & set(friend_data[key][1]))
+        print('%s\t%s' % (key ,inter))
+        
+        
