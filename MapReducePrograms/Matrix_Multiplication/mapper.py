6a7a9b6afde54b61e377a80ca7f7c26cb6211aab
shivammmathur4 <shivamm.4aug@gmail.com>
2021-08-19 18:38:23 +0530

    [Shivam] Refactor : readme and mapper file

--- MapReducePrograms/Matrix_Multiplication/mapper.py
+++ MapReducePrograms/Matrix_Multiplication/mapper.py
@@ -15,10 +15,12 @@ for line in sys.stdin:
 	el=map(int,line.split())
 	if(i<m_r):
 		for j in range(len(el)):
-			for k in range(n_c): print "{0}\t{1}\t{2}\t{3}".format(i, k, j, el[j])
+			for k in range(n_c): 
+			    print("{0}\t{1}\t{2}\t{3}".format(i, k, j, el[j]))
 	else:
 		for j in range(len(el)):
-			for k in range(m_r): print "{0}\t{1}\t{2}\t{3}".format(k, j, i-m_r, el[j])
+			for k in range(m_r): 
+				print("{0}\t{1}\t{2}\t{3}".format(k, j, i-m_r, el[j]))
 	i=i+1
 
 
